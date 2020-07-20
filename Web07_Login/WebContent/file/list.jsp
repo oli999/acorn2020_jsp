569b620dffd0cb37d19560f005ac90105fda0dcd
kimgura <oli999@naver.com>
2020-07-20 12:27:02 +0900

    Web07_Login 에 자료실 페이징 처리중...

--- Web07_Login/WebContent/file/list.jsp
+++ Web07_Login/WebContent/file/list.jsp
@@ -11,10 +11,49 @@
 </head>
 <body>
 <% 
-	//파일 목록
-	List<FileDto> list=FileDao.getInstance().getList();
+	
 	//로그인 된 아이디 읽어오기 (로그인을 하지 않으면 null 이다)
 	String id=(String)session.getAttribute("id");
+	
+	//한 페이지에 나타낼 row 의 갯수
+	final int PAGE_ROW_COUNT=5;
+	//하단 디스플레이 페이지 갯수
+	final int PAGE_DISPLAY_COUNT=5;
+	
+	//보여줄 페이지의 번호
+	int pageNum=1;
+	//보여줄 페이지의 번호가 파라미터로 전달되는지 읽어와 본다.	
+	String strPageNum=request.getParameter("pageNum");
+	if(strPageNum != null){//페이지 번호가 파라미터로 넘어온다면
+		//페이지 번호를 설정한다.
+		pageNum=Integer.parseInt(strPageNum);
+	}
+	//보여줄 페이지 데이터의 시작 ResultSet row 번호
+	int startRowNum=1+(pageNum-1)*PAGE_ROW_COUNT;
+	//보여줄 페이지 데이터의 끝 ResultSet row 번호
+	int endRowNum=pageNum*PAGE_ROW_COUNT;
+	
+	//전체 row 의 갯수를 읽어온다.
+	int totalRow=FileDao.getInstance().getCount();
+	//전체 페이지의 갯수 구하기
+	int totalPageCount=
+			(int)Math.ceil(totalRow/(double)PAGE_ROW_COUNT);
+	//시작 페이지 번호
+	int startPageNum=
+		1+((pageNum-1)/PAGE_DISPLAY_COUNT)*PAGE_DISPLAY_COUNT;
+	//끝 페이지 번호
+	int endPageNum=startPageNum+PAGE_DISPLAY_COUNT-1;
+	//끝 페이지 번호가 잘못된 값이라면 
+	if(totalPageCount < endPageNum){
+		endPageNum=totalPageCount; //보정해준다. 
+	}
+	//startRowNum 과 endRowNum을 FileDto 객체에 담고 
+	FileDto dto=new FileDto();
+	dto.setStartRowNum(startRowNum);
+	dto.setEndRowNum(endRowNum);
+	//FileDto 객체를 인자로 전달해서 파일 목록을 얻어온다. 
+	List<FileDto> list=FileDao.getInstance().getList(dto);
+	
 %>
 <div class="container">
 	<h1>파일 목록입니다.</h1>
--- Web07_Login/src/test/file/dao/FileDao.java
+++ Web07_Login/src/test/file/dao/FileDao.java
@@ -18,6 +18,47 @@ public class FileDao {
 		}
 		return dao;
 	}
+	//전체 row 의 갯수를 리턴해주는 메소드
+	public int getCount() {
+		//전체 row  의 갯수를 담을 지역 변수 
+		int count=0;
+		//필요한 객체의 참조값을 담을 지역변수 만들기 
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			//Connection 객체의 참조값 얻어오기 
+			conn = new DbcpBean().getConn();
+			//ROWNUM 중에서 가장 큰 숫자를 얻어오면 전체 row  의 갯수가 된다. 
+			//혹시 row 가 하나도 없으면 null 이 얻어와 지기때문에  null 인 경우 0 으로 
+			//바꿔 줘야 한다.
+			String sql = "SELECT NVL(MAX(ROWNUM), 0) AS num"
+					+ " FROM board_file";
+			pstmt = conn.prepareStatement(sql);
+			//sql 문에 ? 에 바인딩할 값이 있으면 바인딩하고 
+
+			//select 문 수행하고 결과 받아오기 
+			rs = pstmt.executeQuery();
+			//결과 값 추출하기 
+			if (rs.next()) {
+				count=rs.getInt("num");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return count;
+	}
+	
 	//인자로 잔달되는 파일 번호에 해당하는 파일 정보를 삭제하는 메소드
 	public boolean delete(int num) {
 		Connection conn = null;
@@ -100,7 +141,7 @@ public class FileDao {
 	}
 	
 	//파일 목록을 리턴해주는 메소드
-	public List<FileDto> getList(){
+	public List<FileDto> getList(FileDto dto){
 		//파일 목록을 담을 ArrayList  객체 생성 
 		List<FileDto> list=new ArrayList<>();
 		//필요한 객체의 참조값을 담을 지역변수 만들기 
@@ -111,28 +152,31 @@ public class FileDao {
 			//Connection 객체의 참조값 얻어오기 
 			conn = new DbcpBean().getConn();
 			//실행할 sql 문 준비하기
-			String sql = "SELECT num,writer,title,orgFileName,"
-					+ "saveFileName,fileSize,regdate"
-					+ " FROM board_file"
-					+ " ORDER BY num DESC";
+			String sql = "SELECT *"
+					+ " FROM"
+					+ "     (SELECT result1.*, ROWNUM AS rnum"
+					+ "      FROM (SELECT num,writer,title,orgFileName,fileSize,regdate"
+					+ "            FROM board_file"
+					+ "            ORDER BY num DESC) result1)"
+					+ " WHERE rnum BETWEEN ? AND ?";
 			pstmt = conn.prepareStatement(sql);
 			//sql 문에 ? 에 바인딩할 값이 있으면 바인딩하고 
-			
+			pstmt.setInt(1, dto.getStartRowNum());
+			pstmt.setInt(2, dto.getEndRowNum());
 			//select 문 수행하고 결과 받아오기 
 			rs = pstmt.executeQuery();
 			//반복문 돌면서 결과 값 추출하기 
 			while (rs.next()) {
 				//select 된 파일의 정보를 FileDto 객체에 담고 
-				FileDto dto=new FileDto();
-				dto.setNum(rs.getInt("num"));
-				dto.setWriter(rs.getString("writer"));
-				dto.setTitle(rs.getString("title"));
-				dto.setOrgFileName(rs.getString("orgFileName"));
-				dto.setSaveFileName(rs.getString("saveFileName"));
-				dto.setFileSize(rs.getLong("fileSize"));
-				dto.setRegdate(rs.getString("regdate"));
+				FileDto tmp=new FileDto();
+				tmp.setNum(rs.getInt("num"));
+				tmp.setWriter(rs.getString("writer"));
+				tmp.setTitle(rs.getString("title"));
+				tmp.setOrgFileName(rs.getString("orgFileName"));
+				tmp.setFileSize(rs.getLong("fileSize"));
+				tmp.setRegdate(rs.getString("regdate"));
 				//ArrayList 객체에 누적 시킨다. 
-				list.add(dto);
+				list.add(tmp);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
